b21f914f0c0e211f255754659d225b55595647a9
palkesh pund <palkeshpund1111@gmail.com>
2024-11-23 14:18:46 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Todo from './Components/Todo';
+import Todos from './Components/Todos';
+
+function App() {
+
+
+  return (
+    <>
+      <Todo/>
+      {/* <Todos/> */}
+    </>
+  )
+}
+
+export default App
